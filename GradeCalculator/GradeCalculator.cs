73dc22cb0dfe69c36698f3b92d397ba59c6142e8
Appatechlab <appatibenedict@gmail.com>
2025-08-02 10:29:52 +0000

    Grading Calculator done in CS

---
+++ GradeCalculator/GradeCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class GradeCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your grade (0-100): ");
+
+        string input = Console.ReadLine();
+        int grade;
+
+        if (int.TryParse(input, out grade))
+        {
+            if (grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Please enter a valid grade between 0 and 100.");
+            }
+            else
+            {
+                char letter;
+
+                if (grade >= 90)
+                    letter = 'A';
+                else if (grade >= 80)
+                    letter = 'B';
+                else if (grade >= 70)
+                    letter = 'C';
+                else if (grade >= 60)
+                    letter = 'D';
+                else
+                    letter = 'F';
+
+                Console.WriteLine($"Your letter grade is: {letter}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a numerical value.");
+        }
+    }
+}
